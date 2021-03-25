58fa19ebc453e641fd9835bc162d910d0f22c078
DROW900 <cmunozc1500@alumno.ipn.mx>
2021-03-24 23:11:09 -0600

    meh commit

---
+++ catalogoTramitesServidor.php
@@ -0,0 +1,36 @@
+<?php 
+include 'Funciones/Funciones.php';
+require_once 'Funciones/conexion.php';
+obtenerDatosServidor($_SESSION['idUsuario'],$conection,"servidorpublico");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Buscar Expediente</title>
+		<meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />
+	    <link rel="stylesheet" href="css/normalize.css">
+	    <link rel="stylesheet" href="css/catalogoTramites.css">
+	</head>
+	<body>
+		<header class="site-header">
+			<div class="contenedor">
+				<div class="barra">
+					<a href="login.php"><img src="img/Logo.png" alt="Logo"></a>
+					<div>
+                    <h1>Tus trámites</h1>
+                </div>
+                <div class="log">
+                    <a href="Funciones/cerrarSesion.php">Cerrar Sesión</a>
+                </div>
+				</div>
+			</div>
+		</header>
+
+		<main class="seccion contenedor">
+			<div class="seccionespecial">
+				<?php obtenerTramites($conection, $_SESSION['idTramitante']);?>
+			</div>
+		</main>
+		<footer class="footer-site seccion"></footer>
+	</body>
+</html>	
